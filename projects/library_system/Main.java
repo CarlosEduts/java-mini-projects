e358404835cc226cc5c71d140dc55f6886ed1ed7
Carlos Eduardo <cedut.dev@gmail.com>
2025-07-28 14:28:30 -0300

    📝 docs: adiciona descrição do projeto

--- projects/library_system/Main.java
+++ projects/library_system/Main.java
@@ -1,5 +1,14 @@
 package projects.library_system;
 
+/* 
+ * Projeto: Sistema de Biblioteca
+ * Objetivos:
+ *  - Entender e aplicar herança entre classes.
+ *  - Usar polimorfismo com sobrescrita de métodos.
+ *  - Criar uma hierarquia simples entre classes com comportamentos diferentes.
+ * Data de criação: 27/07/2025
+*/
+
 public class Main {
     public static void main(String[] args) {
         LibraryItem book = new Book("1984", 1949, "George Orwell");
