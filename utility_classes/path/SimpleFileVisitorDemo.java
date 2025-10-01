2d7733673208ee63bc782a92ff7bcb837ca73c38
Carlos Eduardo <cedut.dev@gmail.com>
2025-10-01 16:29:30 -0300

    📝 docs(path): adiciona exemplos de com resgatar o conteúdo de um diretório

---
+++ utility_classes/path/SimpleFileVisitorDemo.java
@@ -0,0 +1,41 @@
+package utility_classes.path;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/*
+ * Conteúdo: Classes Utilitárias 
+ * Caso de uso: Trabalhando com caminhos/diretórios, classe SimpleFileVisitor para resgatar o conteúdo de um diretório recursivamente
+ * Data de criação: 01/09/2025
+ */
+
+class ListAllFiles extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        System.out.println("File name: " + file.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+        System.out.println("\nDirectory: " + dir);
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+public class SimpleFileVisitorDemo {
+    public static void main(String[] args) {
+        Path dir = Path.of("error_handling"); // Diretório atual
+
+        try {
+            Files.walkFileTree(dir, new ListAllFiles());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
