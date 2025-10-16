1ed7b8e2503b8b201d86a5ab2fad2e042a9cb4a7
Carlos Eduardo <cedut.dev@gmail.com>
2025-10-16 20:00:36 -0300

    🐛 fix(naming): ajusta nome da classe para seguir convençã

--- snippets/streams/CollectorsDemo.java
+++ snippets/streams/CollectorsDemo.java
@@ -14,12 +14,12 @@ enum Color {
     RED, GREEN, BLUE
 }
 
-class vehicle {
+class Vehicle {
     String name;
     int price;
     Color color;
 
-    vehicle(String name, int price, Color color) {
+    Vehicle(String name, int price, Color color) {
         this.name = name;
         this.price = price;
         this.color = color;
@@ -38,19 +38,19 @@ public class CollectorsDemo {
         IntSummaryStatistics numbsCollect = numbs.stream().collect(Collectors.summarizingInt(Integer::intValue));
         System.out.println(numbsCollect);
 
-        List<vehicle> vehicles = List.of(
-                new vehicle("Carro", 20000, Color.RED),
-                new vehicle("Bicicleta", 1500, Color.GREEN),
-                new vehicle("Caminhão", 30000, Color.BLUE),
-                new vehicle("ônibus", 50000, Color.RED),
-                new vehicle("Scooter", 800, Color.GREEN),
-                new vehicle("Motocicleta", 7000, Color.BLUE),
-                new vehicle("Van", 25000, Color.RED)
+        List<Vehicle> Vehicles = List.of(
+                new Vehicle("Carro", 20000, Color.RED),
+                new Vehicle("Bicicleta", 1500, Color.GREEN),
+                new Vehicle("Caminhão", 30000, Color.BLUE),
+                new Vehicle("ônibus", 50000, Color.RED),
+                new Vehicle("Scooter", 800, Color.GREEN),
+                new Vehicle("Motocicleta", 7000, Color.BLUE),
+                new Vehicle("Van", 25000, Color.RED)
         );
 
         System.out.println("\nContagem  do grupo de veículos pela cor ...: ");
-        vehicles.stream()
-                .collect(Collectors.groupingBy(vehicle::getColor, Collectors.counting()))
+        Vehicles.stream()
+                .collect(Collectors.groupingBy(Vehicle::getColor, Collectors.counting()))
                 .forEach((color, aLong) -> System.out.println(color + ": " + aLong + (aLong > 1 ? " veículos" : " veículo")));
     }
 }
